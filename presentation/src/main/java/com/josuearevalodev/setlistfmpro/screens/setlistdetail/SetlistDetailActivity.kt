9886b556be6742be800114ac55b860d3f87e1ab8
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-04-07 13:04:52 +0200

    close #7

---
+++ presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/setlistdetail/SetlistDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.josuearevalodev.setlistfmpro.screens.setlistdetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.josuearevalodev.setlistfmpro.R
+
+class SetlistDetailActivity : AppCompatActivity(R.layout.activity_setlist_detail) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
