9bc83da6839f1bffc03d00c75692014fcf2a30ea
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-04-16 13:12:07 +0200

    Tablename set to CacheArtistEntity & mbid is not nullable now

--- persistance/src/main/java/com/josuearevalodev/persistance/entities/CacheArtistEntity.kt
+++ persistance/src/main/java/com/josuearevalodev/persistance/entities/CacheArtistEntity.kt
@@ -1,11 +1,14 @@
 package com.josuearevalodev.domain.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity
-data class CacheArtistEntity(val mbid: String?,
-                             val name: String?,
-                             val sortName: String?,
-                             val disambiguation: String?,
-                             val url: String?) {
-}
+@Entity(tableName = "artists")
+data class CacheArtistEntity(
+    @PrimaryKey val mbid: String,
+    val name: String?,
+    val sortName: String?,
+    val disambiguation: String?,
+    val url: String?
+)
+
