0d5ccefc518ebbcdc96e10bd7c139b259f3fbf6b
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-04-16 13:14:19 +0200

    For the moment, we're not going to store CacheSearchArtistsResponse in database

--- persistance/src/main/java/com/josuearevalodev/persistance/entities/CacheSearchArtistsResponse.kt
+++ persistance/src/main/java/com/josuearevalodev/persistance/entities/CacheSearchArtistsResponse.kt
@@ -2,9 +2,10 @@ package com.josuearevalodev.domain.entities
 
 import androidx.room.Entity
 
-@Entity
-data class CacheSearchArtistsResponse(val type: String?,
-                                      val itemsPerPage: Int?,
-                                      val page: Int?,
-                                      val total: Int?,
-                                      val artist: List<CacheArtistEntity>?)
+data class CacheSearchArtistsResponse(
+    val type: String?,
+    val itemsPerPage: Int?,
+    val page: Int?,
+    val total: Int?,
+    val artist: List<CacheArtistEntity>?
+)
