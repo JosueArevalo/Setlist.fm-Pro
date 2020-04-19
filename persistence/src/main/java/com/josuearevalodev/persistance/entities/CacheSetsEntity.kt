f827350a3aa956d6bc197b48b2666fba1d7c62b7
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-04-19 17:17:19 +0200

    Persistance module changed to persistence v4

--- persistence/src/main/java/com/josuearevalodev/persistance/entities/CacheSetsEntity.kt
+++
@@ -1,6 +0,0 @@
-package com.josuearevalodev.domain.entities
-
-import androidx.room.Entity
-
-@Entity
-data class CacheSetsEntity(val set: List<CacheSetEntity>)
--- persistence/src/main/java/com/josuearevalodev/persistance/entities/CacheVenueEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.josuearevalodev.domain.entities
-
-import androidx.room.Entity
-
-@Entity
-data class CacheVenueEntity(val id: String?,
-                            val name: String?,
-                            val city: CacheCityEntity?,
-                            val url: String?)
