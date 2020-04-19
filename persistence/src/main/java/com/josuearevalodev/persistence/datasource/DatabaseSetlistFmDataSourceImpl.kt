4f26241fcd675bc8732f54357a6fca05a8ae96e6
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-04-19 17:56:28 +0200

    In DatabaseSetlistFmDataSourceImpl, instead send Database, now we pass DAO

--- persistence/src/main/java/com/josuearevalodev/persistence/datasource/DatabaseSetlistFmDataSourceImpl.kt
+++ persistence/src/main/java/com/josuearevalodev/persistence/datasource/DatabaseSetlistFmDataSourceImpl.kt
@@ -3,15 +3,16 @@ package com.josuearevalodev.persistence.datasource
 import com.josuearevalodev.data.setlistfm.datasource.SetListFmDataSource
 import com.josuearevalodev.domain.entities.ArtistEntity
 import com.josuearevalodev.domain.entities.ArtistSetlistsResponse
+import com.josuearevalodev.persistence.db.SetlistFmDao
 import com.josuearevalodev.persistence.db.SetlistFmDatabase
 import com.josuearevalodev.persistence.mapper.mapToArtistEntity
 import io.reactivex.Single
 
-class DatabaseSetlistFmDataSourceImpl(private val db: SetlistFmDatabase) : SetListFmDataSource {
+class DatabaseSetlistFmDataSourceImpl(private val dbDao: SetlistFmDao) : SetListFmDataSource {
 
     override fun getArtists(artistName: String): Single<List<ArtistEntity>> {
         // TODO
-        return db.setlistFmDao()
+        return dbDao
             .getArtists(artistName)
             .map { it.map { it.mapToArtistEntity } }
 
