44499df9b964727a85420cf981b9ddf06adbe3d2
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-04-19 17:13:59 +0200

    Persistance module changed to persistence v2

--- persistance/src/main/java/com/josuearevalodev/persistance/entities/CacheCoordinatesEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.josuearevalodev.domain.entities
-
-import androidx.room.Entity
-
-@Entity
-data class CacheCoordinatesEntity(val lat: Float?,
-                                  val long: Float?)
--- persistance/src/main/java/com/josuearevalodev/persistance/entities/CacheCountryEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.josuearevalodev.domain.entities
-
-import androidx.room.Entity
-
-@Entity
-data class CacheCountryEntity(val code: String?,
-                              val name: String?)
