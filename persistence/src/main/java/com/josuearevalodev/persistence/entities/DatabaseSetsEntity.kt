c3be30d615a383eae23b560f39d67acc2b21147a
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-04-23 12:49:38 +0200

    In DatabaseSetsEntity, param list has to be nullable

--- persistence/src/main/java/com/josuearevalodev/persistence/entities/DatabaseSetsEntity.kt
+++ persistence/src/main/java/com/josuearevalodev/persistence/entities/DatabaseSetsEntity.kt
@@ -3,4 +3,6 @@ package com.josuearevalodev.domain.entities
 import androidx.room.Entity
 
 @Entity
-data class DatabaseSetsEntity(val set: List<DatabaseSetEntity>)
+data class DatabaseSetsEntity(
+    val set: List<DatabaseSetEntity>?
+)
