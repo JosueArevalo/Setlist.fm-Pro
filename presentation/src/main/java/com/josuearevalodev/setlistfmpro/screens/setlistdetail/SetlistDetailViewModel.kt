48c476814d43e6de38bc0ee42312dd8a4987bdde
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-04-25 13:47:54 +0200

    Setlists shown in screen!

--- presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/setlistdetail/SetlistDetailViewModel.kt
+++ presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/setlistdetail/SetlistDetailViewModel.kt
@@ -1,10 +1,15 @@
 package com.josuearevalodev.setlistfmpro.screens.searchartists
 
+import androidx.lifecycle.LiveData
+import com.josuearevalodev.base.classes.ViewState
+
 interface SetlistDetailViewModel {
 
     var artistId: String
 
     var setlistId: String
+
+    val viewState: LiveData<ViewState>
 
     fun getSetlistDetail()
 }
