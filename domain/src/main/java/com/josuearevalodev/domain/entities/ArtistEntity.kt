3fc519e8cdb3af1cf0233e42468637ae44cd8d61
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-04-26 12:58:39 +0200

    In domain, added a new middle package, to differentiate domains (setlistfm vs location) v2

--- domain/src/main/java/com/josuearevalodev/domain/entities/ArtistEntity.kt
+++
@@ -1,8 +0,0 @@
-package com.josuearevalodev.domain.entities
-
-data class ArtistEntity(val mbid: String = "",
-                        val name: String = "",
-                        val sortName: String = "",
-                        val disambiguation: String = "",
-                        val url: String = "")
-
--- domain/src/main/java/com/josuearevalodev/domain/entities/SetEntity.kt
+++
@@ -1,4 +0,0 @@
-package com.josuearevalodev.domain.entities
-
-data class SetEntity(val song: List<SongEntity>)
-
