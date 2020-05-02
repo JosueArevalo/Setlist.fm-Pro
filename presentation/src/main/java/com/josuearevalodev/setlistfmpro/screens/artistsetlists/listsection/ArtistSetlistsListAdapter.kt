74d1f71cb4a00c147a2226036a4f39db6132ff35
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-02 13:47:21 +0200

    [WIP] Add new LoadingViewHolder in List, to prepare pagination

--- presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/artistsetlists/listsection/ArtistSetlistsListAdapter.kt
+++ presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/artistsetlists/listsection/ArtistSetlistsListAdapter.kt
@@ -6,22 +6,39 @@ import com.josuearevalodev.base_android.extensions.inflate
 import com.josuearevalodev.domain.setlistfm.entities.SetlistEntity
 import com.josuearevalodev.setlistfmpro.R
 
-class ArtistSetlistsListAdapter() : RecyclerView.Adapter<SetlistListViewHolder>() {
+class ArtistSetlistsListAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val VIEWHOLDER_SETLIST_TYPE = 0
+    private val VIEWHOLDER_LOADING_TYPE = 1
+
+    private var isLoadingVisible: Boolean = false
 
     private val setlists: MutableList<SetlistEntity> = mutableListOf()
 
     var onSetlistClicked: ((SetlistEntity) -> Unit)? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SetlistListViewHolder {
-        return SetlistListViewHolder(parent.inflate(R.layout.cell_setlist), onSetlistClicked)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEWHOLDER_SETLIST_TYPE -> SetlistListViewHolder(parent.inflate(R.layout.cell_setlist), onSetlistClicked)
+            else -> LoadingViewHolder(parent.inflate(R.layout.cell_loading))
+        }
     }
 
-    override fun onBindViewHolder(holder: SetlistListViewHolder, position: Int) {
-        holder.bind(setlists[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is SetlistListViewHolder -> holder.bind(setlists[position])
+        }
     }
 
     override fun getItemCount(): Int {
         return setlists.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (isLoadingVisible) {
+            true -> VIEWHOLDER_LOADING_TYPE
+            false -> VIEWHOLDER_SETLIST_TYPE
+        }
     }
 
     fun addSetlists(setlists: List<SetlistEntity>) {
