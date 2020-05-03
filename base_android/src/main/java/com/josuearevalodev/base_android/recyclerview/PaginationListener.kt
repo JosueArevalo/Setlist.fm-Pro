05727fdbcbdadc997179fb0c5642dfbcef8ac32f
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-03 13:16:57 +0200

    PaginationListener added to base_android module

---
+++ base_android/src/main/java/com/josuearevalodev/base_android/recyclerview/PaginationListener.kt
@@ -0,0 +1,30 @@
+package com.josuearevalodev.base_android.recyclerview
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class PaginationListener(private val layoutManager: LinearLayoutManager) :
+    RecyclerView.OnScrollListener() {
+
+    abstract val PAGE_START: Int
+    abstract val PAGE_SIZE: Int
+    abstract val isLastPage: Boolean
+    abstract val isLoading: Boolean
+    protected abstract fun loadMoreItems()
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+
+        val visibleItemCount: Int = layoutManager.childCount
+        val totalItemCount: Int = layoutManager.itemCount
+        val firstVisibleItemPosition: Int = layoutManager.findFirstVisibleItemPosition()
+
+        if (!isLoading && !isLastPage) {
+            if (visibleItemCount + firstVisibleItemPosition >= totalItemCount &&
+                firstVisibleItemPosition >= 0 &&
+                totalItemCount >= PAGE_SIZE) {
+                loadMoreItems()
+            }
+        }
+    }
+}
