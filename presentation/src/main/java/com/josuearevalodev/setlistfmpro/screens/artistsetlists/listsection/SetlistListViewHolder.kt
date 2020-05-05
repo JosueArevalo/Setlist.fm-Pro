08f054e0408a74ddbcda0d05616054da59b8b373
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-05 13:31:12 +0200

    In cell, title is bold & underlined

--- presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/artistsetlists/listsection/SetlistListViewHolder.kt
+++ presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/artistsetlists/listsection/SetlistListViewHolder.kt
@@ -1,18 +1,21 @@
 package com.josuearevalodev.setlistfmpro.screens.artistsetlists.listsection
 
+import android.graphics.Paint
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.josuearevalodev.domain.setlistfm.entities.Setlist
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.cell_setlist.view.*
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.*
+
 
 class SetlistListViewHolder(override val containerView: View, private val onSetlistClicked: ((Setlist) -> Unit)? = null) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
     fun bind(setlist: Setlist) {
-        itemView.tvVenueTitle.text = "${setlist.venue.name} , ${setlist.venue.city.name} , ${setlist.venue.city.country.name}"
+        with (itemView.tvVenueTitle) {
+            text = "${setlist.venue.name} , ${setlist.venue.city.name} , ${setlist.venue.city.country.name}"
+            paintFlags = (paintFlags or Paint.UNDERLINE_TEXT_FLAG)
+        }
+
         itemView.tvSongsSummary.text = setlist.songSummary
 
         itemView.tvMonth.text = setlist.month
