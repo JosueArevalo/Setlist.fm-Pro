721d906c38a6aaa078f3dcc2c6566f8e0bd875ee
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-05 12:20:46 +0200

    [WIP] - Some improvement in setlist cell

--- presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/artistsetlists/listsection/SetlistListViewHolder.kt
+++ presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/artistsetlists/listsection/SetlistListViewHolder.kt
@@ -5,15 +5,56 @@ import androidx.recyclerview.widget.RecyclerView
 import com.josuearevalodev.domain.setlistfm.entities.SetlistEntity
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.cell_setlist.view.*
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SetlistListViewHolder(override val containerView: View, private val onSetlistClicked: ((SetlistEntity) -> Unit)? = null) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
     fun bind(setlistEntity: SetlistEntity) {
         itemView.tvVenueTitle.text = "${setlistEntity.venue.name} , ${setlistEntity.venue.city.name} , ${setlistEntity.venue.city.country.name}"
-        itemView.tvVenueDate.text = setlistEntity.eventDate
+        itemView.tvSongsSummary.text = setlistEntity.getSongSummary
+
+        val splittedDate = setlistEntity.eventDate.getSplittedDate("dd-MM-yyyy")
+        itemView.tvMonth.text = splittedDate.first
+        itemView.tvDay.text = splittedDate.second
+        itemView.tvYear.text = splittedDate.third
 
         itemView.setOnClickListener {
             onSetlistClicked?.invoke(setlistEntity)
         }
     }
+
+    private fun String.getSplittedDate(dateFormat: String): Triple<String, String, String> {
+
+        val format = SimpleDateFormat(dateFormat)
+        try {
+            val date: Date = format.parse(this)
+            System.out.println(date)
+
+            val newDateFormat = SimpleDateFormat("dd-MMM-yyyy")
+            val newFormat = newDateFormat.format(date)
+            val elements = newFormat.split("-")
+            return Triple(elements[1], elements[0], elements[2])
+
+
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+
+        return Triple("-", "-", "-")
+    }
+
+    private val SetlistEntity.getSongSummary: String
+        get() {
+            var out = ""
+            sets.set.forEach { setlist ->
+                setlist.song.forEach { song ->
+                    out += "${song.name}, "
+                }
+            }
+            return out
+        }
+
+
 }
