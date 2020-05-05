4f0dedea54c6730e37d46af68411f06fa15da090
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-05 12:23:59 +0200

    ArtistSetlistResponse renamed to ArtistSetlistResponseEntity (deletions)

--- domain/src/main/java/com/josuearevalodev/domain/setlistfm/entities/ArtistSetlistsResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.josuearevalodev.domain.setlistfm.entities
-
-data class ArtistSetlistsResponse(
-    val type: String = "",
-    val itemsPerPage: Int = 0,
-    val page: Int = 0,
-    val total: Int = 0,
-    val setlist: List<SetlistEntity> = listOf()
-)
