0a581f7fef18cf2d9125af9a76ad4add190efb45
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-05 12:27:38 +0200

    - DatabaseArtistSetlistResponse renamed to DatabaseArtistSetlistResponseEntity (deletions) - DatabaseSearchArtistResponse renamed to DatabaseSearchArtistResponseEntity (deletions)

--- persistence/src/main/java/com/josuearevalodev/persistence/setlistfm/entities/DatabaseSearchArtistsResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.josuearevalodev.persistence.setlistfm.entities
-
-data class DatabaseSearchArtistsResponse(
-    val type: String?,
-    val itemsPerPage: Int?,
-    val page: Int?,
-    val total: Int?,
-    val artist: List<DatabaseArtistEntity>?
-)
