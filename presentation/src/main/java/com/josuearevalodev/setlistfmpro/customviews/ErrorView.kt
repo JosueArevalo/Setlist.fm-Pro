e3169518803ca4b74fd52b0b7d6b6bde4d5ba1d0
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-11 12:30:57 +0200

    ViewBinding added inArtistSetlistsListFragment

--- presentation/src/main/java/com/josuearevalodev/setlistfmpro/customviews/ErrorView.kt
+++ presentation/src/main/java/com/josuearevalodev/setlistfmpro/customviews/ErrorView.kt
@@ -2,14 +2,20 @@ package com.josuearevalodev.setlistfmpro.customviews
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.button.MaterialButton
 import com.josuearevalodev.base_android.extensions.gone
-import com.josuearevalodev.setlistfmpro.R
+import com.josuearevalodev.setlistfmpro.databinding.ViewErrorBinding
 
 class ErrorView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : ConstraintLayout(context, attrs) {
 
+    private var binding: ViewErrorBinding
+    val bRetry: MaterialButton
+        get() = binding.bRetry
+
     init {
-        inflate(context, R.layout.view_error, this)
+        binding = ViewErrorBinding.inflate(LayoutInflater.from(context), this, true)
         gone()
     }
 }
