02ded8278b0d92e2f30b116a30188dbd24d1f1d9
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-11 12:37:12 +0200

    ViewBinding added in ArtistSetlistsMapFragment

--- presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/artistsetlists/mapsection/ArtistSetlistsMapFragment.kt
+++ presentation/src/main/java/com/josuearevalodev/setlistfmpro/screens/artistsetlists/mapsection/ArtistSetlistsMapFragment.kt
@@ -1,7 +1,9 @@
 package com.josuearevalodev.setlistfmpro.screens.artistsetlists.mapsection
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -9,28 +11,45 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.josuearevalodev.setlistfmpro.R
+import com.josuearevalodev.setlistfmpro.databinding.FragmentArtistSetlistsMapBinding
 import com.josuearevalodev.setlistfmpro.screens.artistsetlists.shared.ArtistSetlistsSharedViewModel
 import com.josuearevalodev.setlistfmpro.screens.artistsetlists.shared.ArtistSetlistsSharedViewModelImpl
-import kotlinx.android.synthetic.main.fragment_artist_setlists_map.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
-class ArtistSetlistsMapFragment : Fragment(R.layout.fragment_artist_setlists_map), OnMapReadyCallback {
+class ArtistSetlistsMapFragment : Fragment(), OnMapReadyCallback {
 
-    val sharedViewModel: ArtistSetlistsSharedViewModel by sharedViewModel<ArtistSetlistsSharedViewModelImpl>()
+    private var _binding: FragmentArtistSetlistsMapBinding? = null
+    val binding get() = _binding!!
 
+    private val sharedViewModel: ArtistSetlistsSharedViewModel by sharedViewModel<ArtistSetlistsSharedViewModelImpl>()
     private var googleMap: GoogleMap? = null
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        mvMap.onCreate(savedInstanceState)
-        mvMap.onResume()
-        mvMap.getMapAsync(this)
+
+        binding.mvMap.onCreate(savedInstanceState)
+        binding.mvMap.onResume()
+        binding.mvMap.getMapAsync(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentArtistSetlistsMapBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         addObservers()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     private fun addObservers() {
