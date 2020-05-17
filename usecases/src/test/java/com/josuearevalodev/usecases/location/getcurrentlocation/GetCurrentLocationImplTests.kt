2510b68f328e90e4e1e323ea3c232dea3919e6cd
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-17 12:40:46 +0200

    GetCurrentLocationImplTests - Testing that entity returned from Repository is the same that returned from use case

--- usecases/src/test/java/com/josuearevalodev/usecases/location/getcurrentlocation/GetCurrentLocationImplTests.kt
+++ usecases/src/test/java/com/josuearevalodev/usecases/location/getcurrentlocation/GetCurrentLocationImplTests.kt
@@ -1,7 +1,10 @@
 package com.josuearevalodev.usecases.location.getcurrentlocation
 
+import com.josuearevalodev.domain.location.entities.LocationEntity
 import com.josuearevalodev.domain.location.repository.LocationRepository
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Maybe
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -36,4 +39,20 @@ class GetCurrentLocationImplTests {
         verify(locationRepository).getCurrentLocation()
     }
 
+    @Test
+    fun `invoke returns same Location entity than repository`() {
+        // Given (None)
+        val fakeMaybeLocation = Maybe.just(LocationEntity(
+            longitude = 42.0f,
+            latitude = 2.0f
+        ))
+
+        whenever(locationRepository.getCurrentLocation()).thenReturn(fakeMaybeLocation)
+
+        // When
+        val result = getCurrentLocation()
+
+        // Then
+        Assert.assertEquals(fakeMaybeLocation, result)
+    }
 }
