2cc50be51ee0ef8da79871cd85d10b3e2346219a
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-20 22:52:04 +0200

    Simple empty test

---
+++ data/src/test/java/com/josuearevalodev/data/setlistfm/repository/SetListFmRepositoryImplTests.kt
@@ -0,0 +1,36 @@
+package com.josuearevalodev.data.setlistfm.repository
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class SetListFmRepositoryImplTests {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getArtist() {
+        assertTrue(true);
+    }
+
+    @Test
+    fun getArtistSetlists() {
+        assertTrue(true);
+    }
+
+    @Test
+    fun getSetlistDetail() {
+        assertTrue(true);
+    }
+
+    @Test
+    fun updateArtistWithSetlistsHeaderData() {
+        assertTrue(true);
+    }
+}
