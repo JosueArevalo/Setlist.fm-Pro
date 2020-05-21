690696a6867fbfb195d2252d8664a86a5a8e6023
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-05-21 12:54:00 +0200

    SetlistFmRepositoryImplTests - Some tests

--- data/src/test/java/com/josuearevalodev/data/setlistfm/repository/SetListFmRepositoryImplTests.kt
+++ data/src/test/java/com/josuearevalodev/data/setlistfm/repository/SetListFmRepositoryImplTests.kt
@@ -1,22 +1,70 @@
 package com.josuearevalodev.data.setlistfm.repository
 
+import com.josuearevalodev.data.setlistfm.datasource.SetListFmDatabaseDataSource
+import com.josuearevalodev.data.setlistfm.datasource.SetListFmRemoteDataSource
+import com.josuearevalodev.domain.setlistfm.entities.ArtistEntity
+import com.josuearevalodev.domain.setlistfm.repository.SetListFmRepository
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class SetListFmRepositoryImplTests {
 
+    @Mock
+    private lateinit var databaseDS: SetListFmDatabaseDataSource
+
+    @Mock
+    private lateinit var remoteDS: SetListFmRemoteDataSource
+
+    private lateinit var setListFmRepository: SetListFmRepository
+
     @Before
     fun setUp() {
+        setListFmRepository = SetListFmRepositoryImpl(
+            databaseDS = databaseDS,
+            remoteDS = remoteDS
+        )
     }
 
     @Test
-    fun getArtist() {
-        assertTrue(true);
+    fun `try to get artist - databaseDS getArtist() is called`() {
+
+        // Given
+        val artistName = "Artist Name"
+        whenever(databaseDS.getArtist(artistName)).thenReturn(Single.just(ArtistEntity(name = "Artist Name")))
+
+        // When
+        val result = setListFmRepository.getArtist(artistName = artistName)
+
+        // Then
+        verify(databaseDS).getArtist(artistName = artistName)
+    }
+
+    @Test
+    fun `try to get artist - Got error from databaseDS - RemoteDS getArtist() called`() {
+        // Given
+        val artistName = "Artist Name"
+        whenever(databaseDS.getArtist(any())).thenReturn(Single.error(Throwable()))
+
+        // When
+        val test = setListFmRepository.getArtist(artistName = artistName).test()
+        test.awaitTerminalEvent()
+
+        // Then
+        test.assertError(Throwable::class.java)
+
+        verify(databaseDS).getArtist(artistName)
+        verify(remoteDS).getArtist(artistName)
+        test.dispose()
     }
 
     @Test
