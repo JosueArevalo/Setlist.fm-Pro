e675bf8cd8eaa3d553f31a7d9c07ed79891b1e13
Josué Arévalo <josue.arevalo.dev@gmail.com>
2020-06-01 16:34:45 +0200

    Assert that header is displayed

--- presentation/src/androidTest/java/com/josuearevalodev/setlistfmpro/screens/setlistdetail/SetlistDetailActivityTest.kt
+++ presentation/src/androidTest/java/com/josuearevalodev/setlistfmpro/screens/setlistdetail/SetlistDetailActivityTest.kt
@@ -1,8 +1,14 @@
 package com.josuearevalodev.setlistfmpro.screens.setlistdetail
 
 import android.content.Intent
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import com.josuearevalodev.setlistfmpro.R
 import org.junit.After
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -24,7 +30,7 @@ class SetlistDetailActivityTest {
                 return result
             }
         }
-    
+
     @Before
     fun setUp() {
     }
@@ -35,7 +41,7 @@ class SetlistDetailActivityTest {
 
 
     @Test
-    fun helloWorld() {
-        assertTrue(true)
+    fun testHeaderIsVisible() {
+        onView(withId(R.id.clHeader)).check(matches(isDisplayed()))
     }
 }
